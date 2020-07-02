b9d169ba553ce57de7f9825c4c32510335619c61
Javier <javier.fuentesm@hotmail.com>
2020-07-02 19:11:37 -0400

    Se agrega el balanceador de carga

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     if (err) {
       res.status(500).send('💥 Explotó 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('Me conecté a la DB X2! 😎');
       db.close();
     }
   });
